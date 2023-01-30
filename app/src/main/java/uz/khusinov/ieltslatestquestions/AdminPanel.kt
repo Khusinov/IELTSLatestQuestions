a59ee7f0564c449ac5ade08040e7faa6d5d01633
Khusinov <shaxriyorxusinov2020@gmail.com>
2023-01-31 00:40:46 +0500

    admin panel

---
+++ app/src/main/java/uz/khusinov/ieltslatestquestions/AdminPanel.kt
@@ -0,0 +1,50 @@
+package uz.khusinov.ieltslatestquestions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import uz.khusinov.ieltslatestquestions.databinding.ActivityAdminPanelBinding
+import uz.khusinov.ieltslatestquestions.model.Question
+
+class AdminPanel : AppCompatActivity() {
+    lateinit var binding: ActivityAdminPanelBinding
+    val db = Firebase.firestore
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAdminPanelBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.addButton.setOnClickListener {
+            val day = binding.dayAdd.text.toString().toInt()
+            val month =binding.monthAdd.text.toString().toInt()
+            val year = binding.yearAdd.text.toString().toInt()
+            val type = binding.questionTypeAdd.text.toString().toInt()
+            val imageUrl = ""
+            val questionBody = binding.questionAdd.text.toString()
+
+            val question = Question()
+            question.day = day
+            question.month = month
+            question.year = year
+            question.type = type
+            question.questionBody = questionBody
+            question.imageUrl = imageUrl
+
+
+            db.collection("Question")
+                .add(question)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Added", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener{
+                    Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
+                }
+        }
+
+
+    }
+}
---
+++ app/src/main/java/uz/khusinov/ieltslatestquestions/model/Question.kt
@@ -0,0 +1,12 @@
+package uz.khusinov.ieltslatestquestions.model
+
+data class Question(
+    var id:String = "",
+    var day:Int = 0,
+    var month:Int = 0,
+    var year:Int = 0,
+    var type:Int = 0,
+    var imageUrl:String? = null,
+    var questionBody:String = "A b C d"
+
+)
